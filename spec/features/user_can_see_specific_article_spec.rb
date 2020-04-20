78013397a8b2f6337c40b416ced12a854aceb4af
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-20 19:15:07 +0200

    Article title now links to corresponding content on new page. Passing feature test and extended unit test for model Article

---
+++ spec/features/user_can_see_specific_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User can see a specific article' do
+    before do
+        create(:article, title: 'Breaking news!', content: 'Look at all these people breaking todays news.')
+        create(:article, title: 'Broken news!', content: 'Today, some broken news was found in a back garden')
+
+        visit root_path
+        click_on 'Breaking news!'
+    end
+
+    context 'Article displays' do
+        it 'title' do
+            expect(page).to have_content 'Breaking news!'
+        end
+        
+        it 'content' do
+            expect(page).to have_content 'Look at all these people breaking todays news.'
+        end
+    end
+end
