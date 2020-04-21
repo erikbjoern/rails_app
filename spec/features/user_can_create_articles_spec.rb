201ade23ce6c416d1f2d910da750b5e1c485a0fc
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-21 10:40:14 +0200

    writing feature test for happy path and sad path

---
+++ spec/features/user_can_create_articles_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+feature 'User can create articles' do
+    before do
+        visit root_path
+        click_on 'New Article'
+    end
+
+    context 'Successfully create an article [Happy Path]' do
+        before do
+            fill_in 'Title', with: 'Happy holidays'
+            fill_in 'Content', with: 'Buy your gifts now!'
+            click_on 'Create article'
+        end
+
+        it 'User should be on article show page' do
+            article = Article.find_by(title: 'Happy holidays')
+            expect(current_path).to eq article_path(article)
+        end
+
+        it 'User should see a success message' do
+            expect(page).to have_content 'Your article was successfully created.'
+        end
+        
+        it 'User should see article' do
+            expect(page).to have_content 'Happy holidays'
+        end
+        
+        it 'User should see article content' do
+            expect(page).to have_content 'Buy your gifts now!'
+        end
+    end
+
+    context "User doesn't enter a title for the article [Sad Path]" do
+        before do
+            fill_in 'Content', with: 'Buy your gifts now!'
+            click_on 'Create article'
+        end
+
+        it 'User should see error message' do
+            expect(page).to have_content 'Your article needs a title.'
+        end
+    end 
+
+    context "User doesn't enter content for the article [Sad Path]" do
+        before do
+            fill_in 'Title', with: 'Happy holidays'
+            click_on 'Create article'
+        end
+    
+        it 'User should see error message' do
+            expect(page).to have_content 'Your article needs content.'
+        end
+    end 
+    
+    context "User doesn't enter anything at all [Sad Path]" do
+        before do
+            click_on 'Create article'
+        end
+    
+        it 'User should see error message' do
+            expect(page).to have_content 'Your article needs a title and content.'
+        end
+    end 
+end
