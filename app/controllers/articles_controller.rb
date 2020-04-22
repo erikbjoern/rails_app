5d3de9360b0718315c2acfd77fdeb50066da5380
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-22 09:08:54 +0200

    Creating new view 'new', with a form for creating new article. Link to 'new' view from 'index' view. defining 'new' and 'create' in ArticlesController, and adding actions to route.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
 end
