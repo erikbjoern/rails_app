35a614eaf95cff8d2bf77ae833d8830fcf754d05
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-25 18:29:43 +0200

    writing unit test for user, adding email and encrypted_password to factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'user@email' }
+    encrypted_password { 'userpassword' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :password_confirmation }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :encrypted_password }
+  end
+
+  describe 'Factory' do
+    it 'should have a valid factory' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end
+end  
