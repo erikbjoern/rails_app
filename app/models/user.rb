781cc3546077c31c24168d8ca6ae0768c7418b2b
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-25 20:34:32 +0200

    correcting factory to pass in password instead of encrypted, adding validations to User model and passing unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :email
+  validates_presence_of :encrypted_password
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     email { 'user@email' }
-    encrypted_password { 'userpassword' }
+    password { 'userpassword' }
   end
 end
