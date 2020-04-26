8f36f47f8fb98060d1e050a8614dcaab321b1b56
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-26 22:10:05 +0200

    testing registered vs. unregistered user managing articles

---
+++ spec/features/registered_user_can_manage_articles_spec.rb
@@ -0,0 +1,57 @@
+feature 'registered user can manage articles' do
+    before do
+        create(:article, title: 'Breaking news!', content: 'Look at all these people breaking todays news.')
+    end
+
+    context 'registered user' do
+        let(:user) { create(:user) }
+        before do
+            login_as(user, scope: :user)
+            visit root_path
+        end
+            
+        it 'can create article' do
+            click_on 'New Article'
+            fill_in 'article_title', with: 'The Title'
+            fill_in 'article_content', with: 'The content.'
+            click_on 'commit'
+            expect(page).to have_content 'Your article was successfully created.'
+        end
+
+        it 'can edit article' do
+            article = Article.find_by_title('Breaking news!')
+            visit article_path(article)
+            click_on 'Edit Article'
+            fill_in 'article_title', with: 'This is edited'
+            click_on 'Save'
+            expect(page).to have_content 'This is edited'
+        end
+
+        it 'can delete article' do
+            article = Article.find_by_title('Breaking news!')
+            visit article_path(article)
+            click_on 'Delete Article'
+            expect(page).to have_content 'Your article was successfully deleted.'
+        end
+    end
+
+    context 'unregistered user' do
+        
+        it 'cannot see link to create new article on index page' do
+            visit root_path
+            expect(page).not_to have_content 'New Article'
+        end
+
+        it 'cannot see link to edit article on article page' do
+            article = Article.find_by_title('Breaking news!')
+            visit article_path(article)
+            expect(page).not_to have_content 'Edit Article'  
+        end
+        
+        it 'cannot see link to delete article on article page' do
+            article = Article.find_by_title('Breaking news!')
+            visit article_path(article)
+            expect(page).not_to have_content 'Delete Article'  
+        end
+    end
+end
