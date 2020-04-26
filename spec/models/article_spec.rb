75976fdef1ca1558726b266db274e4303e2f9d41
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-26 20:18:30 +0200

    adding author_id to article_spec, adding db table column author_id

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,11 +3,13 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :author_id}
   end
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :content }
+    it { is_expected.to validate_presence_of :author_id }
   end
 
   describe 'Factory' do
